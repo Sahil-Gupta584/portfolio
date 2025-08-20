6ea8262b5b448dfaef17ad75b33db7dceb17afdf
Sahil Gupta <guptas3067@gmail.com>
2025-08-20 16:44:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  // experimental:{
-  //   dynamicIO: true,
-  // }
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["books-mall.vercel.app"],
+    },
+  },
 };
 
 export default nextConfig;
