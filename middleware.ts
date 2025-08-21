e871fb000faeeb737ea052f249b5dca71046ce07
Sahil Gupta <guptas3067@gmail.com>
2025-08-21 15:44:47 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,22 @@
-// middleware.ts (inside your sahil-alpha project root)
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const res = NextResponse.next();
+  const booksmallFrontendUrl = process.env.BOOKSMALL_FRONTEND_URL;
+  console.log("BOOKSMALL_FRONTEND_URL:", booksmallFrontendUrl);
 
-  res.headers.set("Access-Control-Allow-Origin", "https://books-mall.vercel.app");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // res.headers.set("Access-Control-Allow-Origin", "https://books-mall.vercel.app");
+  res.headers.set("Access-Control-Allow-Origin", booksmallFrontendUrl || "");
+  res.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.headers.set("Access-Control-Allow-Credentials", "true");
 
   // If it's a preflight request, respond immediately
   if (req.method === "OPTIONS") {
